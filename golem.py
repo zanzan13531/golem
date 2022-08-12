844b3e6cce8aba16677aacdf5999f8b116663089
zanzan13531 <zanzan13531@gmail.com>
2022-08-11 23:50:59 -0700

    got rid of unecessary pytorch stuff

--- golem.py
+++ golem.py
@@ -1,4 +1,5 @@
 import torch
+import math
 
 # I guess everything uses the pytorch datastructure of a Tensor? (basically a matrix)
 
@@ -8,8 +9,7 @@ import torch
 def golemify(B, x): 
 
     # first half: 
-    firstHalf = torch.tensor()
-
+    firstHalf = 0
     d = x.size(dim = 0) # d : number of variables in x
     n = x.size(dim = 1); # n : number of data points per variable in x
 
@@ -29,11 +29,11 @@ def golemify(B, x):
             innerThing = torch.mul(innerThingB, innerThingC) # B^T_i * x^k : matrix multiplication of the two previous parts
             innerThing = innerThingA - innerThingB # x^k_i - B^T_i * x^k : entire thing except for the square
             innerThing = innerThing ** 2.0 # squared
-            sum = torch.add(sum, innerThing) # adds the inner stuff to them sum
+            sum = sum + innerThing # adds the inner stuff to them sum
 
-    logSum = torch.log(sum) # log(sum stuff) : log of the sum stuff
+    logSum = math.log(sum) # log(sum stuff) : log of the sum stuff
 
-    firstHalf = torch.mul(logSum, d/2.0) # d*log(sum stuff)/2 : first half of the equation, pretty sure if this is a scalar
+    firstHalf = logSum * d / 2.0 # d*log(sum stuff)/2 : first half of the equation, pretty sure if this is a scalar
 
     # second half:
 
@@ -41,5 +41,5 @@ def golemify(B, x):
     inner = torch.sub(I, B)  # I - B : subtracting B from the identity matrix
     secondHalf = torch.logdet(inner) # log|det(I - B)| : I think this is a scalar?
 
-    return(torch.sub(firstHalf, secondHalf))
+    return(firstHalf - secondHalf)
 
