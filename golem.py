0f7d095d9aded0db0483839fa6fdb63bf21f9627
zanzan13531 <zanzan13531@gmail.com>
2022-08-16 21:59:02 -0700

    a bunch of refactoring to make it more readable

--- golem.py
+++ golem.py
@@ -6,40 +6,38 @@ import math
 # B = weighted adjacency matrix
 # x = set of variables being represented by the DAG
 # !!! assuming both B is a square of d by d size, and that x is a 2D matrix with dimensions d by x, where there are d variables and n cases of each (basically data points)
-def golemify(B, x): 
+def L2(B, x): 
 
     # first half: 
-    firstHalf = 0
     d = x.size(dim = 0) # d : number of variables in x
     n = x.size(dim = 1); # n : number of data points per variable in x
 
-    sum = 0 # initlaizing sum
+    doubleSum = 0 # initlaizing sum
 
     # !!! this assumes that the sum results in a scalar, not sure if that's right or not
     for i in range (d): # outer sum
         for k in range(n): # inner sum
 
-            innerThingA = x[i, k] # x^k_i : first part which takes the element at index [k, i] of x
+            xki = x[i, k] # x^k_i : first part which takes the element at index [k, i] of x
 
-            innerThingB = B[:, i] # B_i : ith column of B
-            innerThingB = torch.transpose(innerThingB, 0, 1) # B^T_i : ith column of B transposed (rotated 90 degrees)
+            bi = B[:, i] # B_i : ith column of B
+            bti = torch.transpose(bi, 0, 1) # B^T_i : ith column of B transposed (rotated 90 degrees)
 
-            innerThingC = x[k, :] # x^k : kth column of x                                                                       
+            xk = x[k, :] # x^k : kth column of x                                                                       
 
-            innerThing = torch.mul(innerThingB, innerThingC) # B^T_i * x^k : matrix multiplication of the two previous parts
-            innerThing = innerThingA - innerThingB # x^k_i - B^T_i * x^k : entire thing except for the square
-            innerThing = innerThing ** 2.0 # squared
-            sum = sum + innerThing # adds the inner stuff to them sum
+            btixk = torch.mul(bti, xk) # B^T_i * x^k : matrix multiplication of the two previous parts
+            xki_btixk = xki - btixk # x^k_i - B^T_i * x^k : entire thing except for the square
+            xki_btixk_squared = xki_btixk ** 2.0 # squared
+            doubleSum = doubleSum + xki_btixk_squared # adds the inner stuff to them sum
 
-    logSum = math.log(sum) # log(sum stuff) : log of the sum stuff
-
-    firstHalf = logSum * d / 2.0 # d*log(sum stuff)/2 : first half of the equation, pretty sure if this is a scalar
+    logDoubleSum = math.log(doubleSum) # log(sum stuff) : log of the sum stuff
 
     # second half:
-
     I = torch.eye(d) # I : creating identity matrix of the same dimention as B
-    inner = torch.sub(I, B)  # I - B : subtracting B from the identity matrix
-    secondHalf = torch.logdet(inner) # log|det(I - B)| : I think this is a scalar?
+    IB = torch.sub(I, B)  # I - B : subtracting B from the identity matrix
+    logDetIB = torch.logdet(IB) # log|det(I - B)| : I think this is a scalar?
 
-    return(firstHalf - secondHalf)
+    L2Result = logDoubleSum * d / 2.0 - logDetIB # final result of the L2 function
+
+    return(L2Result)
 
