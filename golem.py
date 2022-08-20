a249ff46501ab078fe710d1c7573a042f8e34cde
zanzan13531 <zanzan13531@gmail.com>
2022-08-19 21:42:45 -0700

    added in the other score function (first score function)

--- golem.py
+++ golem.py
@@ -19,6 +19,45 @@ class Net(torch.nn.Module):
 def netFunction(x):
     return (torch.transpose(net.weight * x)) # supposedly the function for net, no idea if it works/how it works
 
+
+# B = weighted adjacency matrix
+# x = set of variables being represented by the DAG
+# !!! assuming both B is a square of d by d size, and that x is a 2D matrix with dimensions d by x, where there are d variables and n cases of each (basically data points)
+def L1(B, x): 
+
+    # first half: 
+    d = x.size(dim = 0) # d : number of variables in x
+    n = x.size(dim = 1); # n : number of data points per variable in x
+
+    doubleSum = 0 # initlaizing sum
+
+    # !!! this assumes that the sum results in a scalar, not sure if that's right or not
+    for i in range (d): # outer sum
+
+        for k in range(n): # inner sum
+
+            xki = x[i, k] # x^k_i : first part which takes the element at index [k, i] of x
+
+            bi = B[:, i] # B_i : ith column of B
+            bti = torch.transpose(bi, 0, 1) # B^T_i : ith column of B transposed (rotated 90 degrees)
+
+            xk = x[k, :] # x^k : kth column of x                                                                       
+
+            btixk = torch.mul(bti, xk) # B^T_i * x^k : matrix multiplication of the two previous parts
+            xki_btixk = xki - btixk # x^k_i - B^T_i * x^k : entire thing except for the square
+            xki_btixk_squared = xki_btixk ** 2.0 # squared
+            doubleSum = doubleSum + math.log(xki_btixk_squared) # takes the log of the inner sum and adds it to the total sum
+
+    # second half:
+    I = torch.eye(d) # I : creating identity matrix of the same dimention as B
+    IB = torch.sub(I, B)  # I - B : subtracting B from the identity matrix
+    logDetIB = torch.logdet(IB) # log|det(I - B)| : I think this is a scalar?
+
+    L1Result = doubleSum / 2.0 - logDetIB # final result of the L2 function
+
+    return(L1Result)
+
+
 # B = weighted adjacency matrix
 # x = set of variables being represented by the DAG
 # !!! assuming both B is a square of d by d size, and that x is a 2D matrix with dimensions d by x, where there are d variables and n cases of each (basically data points)
@@ -57,11 +96,15 @@ def L2(B, x):
 
     return(L2Result)
 
+
 def h(B): #characterization of DAGness function
-    return (torch.trace(torch.matrix_exp(torch.mul(B, B))) - B.size(dim = 0)) # tr(e^(B o B)) - d : trace of the matrix exponential of the hadamard product of B and itself
+    return (torch.trace(torch.matrix_exp(torch.mul(B, B))) - B.size(dim = 0)) # tr(e^(B o B)) - d : trace of the matrix exponential of the hadamard product of B and itself minus d
+
+def scoreFunction1(B, x):
+    return (L1(B, x) + lambda1 * torch.norm(B) + lambda2 * h(B)) # S2(B, x) = L2(B, x) + lambda1 * ||B||_1 + lambda2 * h(B) : first score function
 
 def scoreFunction2(B, x):
-    return (L2(B, x) + lambda1 * torch.norm(B) + lambda2 * h(B)) # S2(B, x) = L2(B, x) + lambda1 * ||B||_1 + lambda2 * h(B) : score function
+    return (L2(B, x) + lambda1 * torch.norm(B) + lambda2 * h(B)) # S2(B, x) = L2(B, x) + lambda1 * ||B||_1 + lambda2 * h(B) : second score function
 
 
 learning_rate = 0.1
