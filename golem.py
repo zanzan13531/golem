8d57d89c331756e71c9fe3c42dfb9e7d8c7bc521
zanzan13531 <zanzan13531@gmail.com>
2022-08-31 00:48:58 -0700

    realized I made a pretty big mistake in the matrix multiplication in the middle where it was resulting in a dxd matrix instead of a 1x1

--- golem.py
+++ golem.py
@@ -34,7 +34,7 @@ class golem():
 
         # first half: 
         d = x.size(dim = 1) # d : number of variables in x
-        n = x.size(dim = 0); # n : number of data points per variable in x
+        n = x.size(dim = 0) # n : number of data points per variable in x
 
 
         doubleSum = 0 # initlaizing sum
@@ -70,12 +70,12 @@ class golem():
 
     # B = weighted adjacency matrix
     # x = set of variables being represented by the DAG
-    # !!! assuming both B is a square of d by d size, and that x is a 2D matrix with dimensions d by x, where there are d variables and n cases of each (basically data points)
+    # !!! assuming both B is a square of d by d size, and that x is a 2D matrix with dimensions n by d, where there are d variables and n cases of each (basically data points)
     def L2(self, B, x): 
 
         # first half: 
+        n = x.size(dim = 0) # n : number of data points per variable in x
         d = x.size(dim = 1) # d : number of variables in x (dimension of b)
-        n = x.size(dim = 0); # n : number of data points per variable in x
 
         doubleSum = 0 # initlaizing sum
 
@@ -108,6 +108,57 @@ class golem():
 
         return(L2Result)
 
+    # B = weighted adjacency matrix
+    # x = set of variables being represented by the DAG
+    # !!! assuming both B is a square of d by d size, and that x is a 2D matrix with dimensions n by d, where there are d variables and n cases of each (basically data points)
+    def L2v2(self, B, x): 
+
+        # first half: 
+        n = x.size(dim = 0) # n : number of data points per variable in x
+        d = x.size(dim = 1) # d : number of variables in x (dimension of b)
+
+        doubleSum = 0 # initlaizing sum
+
+        # !!! this assumes that the sum results in a scalar, not sure if that's right or not
+        for i in range (d): # outer sum
+            for k in range(n): # inner sum
+
+                xki = x[k, i] # x^k_i : first part which takes the element at index [k, i] of x
+                #print(xki)
+
+                bi = B[i, :] # B_i : ith column of B
+                bi = bi[None, :]
+                #print(bi)
+                bti = torch.transpose(bi, 0, 1) # B^T_i : ith column of B transposed (rotated 90 degrees)
+                #print(bti)
+
+                #xk = x[:, k] # x^k : kth column of x   
+                xk = x[k, :]
+                xk = xk[None, :]
+                #print(xk)
+
+                btixk = torch.matmul(xk.float(), bti.float()) # B^T_i * x^k : matrix multiplication of the two previous parts
+                #print(btixk)
+                xki_btixk = xki - btixk # x^k_i - B^T_i * x^k : entire thing except for the square
+                #print(xki_btixk)
+                xki_btixk_squared = xki_btixk ** 2.0 # squared
+                #print(xki_btixk_squared)
+                #return
+                #print(xki_btixk_squared)
+                doubleSum = doubleSum + xki_btixk_squared # adds the inner stuff to them sum
+
+        logDoubleSum = torch.log(doubleSum) # log(sum stuff) : log of the sum stuff
+
+        # second half:
+        I = torch.eye(d) # I : creating identity matrix of the same dimention as B
+        IB = torch.sub(I, B)  # I - B : subtracting B from the identity matrix
+        IB = torch.abs(IB)
+        logDetIB = torch.logdet(IB) # log|det(I - B)| : I think this is a scalar?
+
+        L2Result = logDoubleSum * d / 2.0 - logDetIB # final result of the L2 function
+
+        return(L2Result)
+
 
     def h(self, B): #characterization of DAGness function
         return (torch.trace(torch.matrix_exp(torch.mul(B, B))) - B.size(dim = 0)) # tr(e^(B o B)) - d : trace of the matrix exponential of the hadamard product of B and itself minus d
@@ -116,7 +167,8 @@ class golem():
         return (self.L1(B, x) + self.lambda1 * B.norm(1) + self.lambda2 * self.h(B)) # S2(B, x) = L2(B, x) + lambda1 * ||B||_1 + lambda2 * h(B) : first score function
 
     def scoreFunction2(self, B, x):
-        return (self.L2(B, x) + self.lambda1 * B.norm(1) + self.lambda2 * self.h(B)) # S2(B, x) = L2(B, x) + lambda1 * ||B||_1 + lambda2 * h(B) : second score function
+        #return (self.L2(B, x) + self.lambda1 * B.norm(1) + self.lambda2 * self.h(B)) # S2(B, x) = L2(B, x) + lambda1 * ||B||_1 + lambda2 * h(B) : second score function
+        return (self.L2v2(B, x) + self.lambda1 * B.norm(1) + self.lambda2 * self.h(B)) # S2(B, x) = L2(B, x) + lambda1 * ||B||_1 + lambda2 * h(B) : second score function
 
     def train(self, dataset, epochs=1, scoreFunction=None): #should be batches by x (# of batches by d by n)
         self.net = self.Net(dataset.size(dim=1))
